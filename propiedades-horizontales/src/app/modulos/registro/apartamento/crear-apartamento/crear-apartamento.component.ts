32e139370267566b57d42df8d7018833395f012c
Katherine Arevalo <jenkarevalo@gmail.com>
2022-11-20 19:29:45 -0500

    Correccion Apartamento

--- propiedades-horizontales/src/app/modulos/registro/apartamento/crear-apartamento/crear-apartamento.component.ts
+++ propiedades-horizontales/src/app/modulos/registro/apartamento/crear-apartamento/crear-apartamento.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApartamentoModelo } from 'src/app/modelos/apartamento.modelo';
+import { PropietarioModelo } from 'src/app/modelos/propietario.modelo';
+import { TorreModelo } from 'src/app/modelos/torre.modelo';
 import { ApartamentoService } from 'src/app/servicios/apartamento.service';
+import { PropietarioService } from 'src/app/servicios/propietario.service';
+import { TorreService } from 'src/app/servicios/torre.service';
 
 
 @Component({
@@ -11,15 +15,21 @@ import { ApartamentoService } from 'src/app/servicios/apartamento.service';
   styleUrls: ['./crear-apartamento.component.css']
 })
 export class CrearApartamentoComponent implements OnInit {
+  listadoTorre: TorreModelo [] = [];
+  listadoPropietarios: PropietarioModelo [] = [];
+
   formApartamento: FormGroup = this.formBuilder.group({
     'Numero':['',[Validators.required]],
     'TorreId':['',[Validators.required]],
     'PropietarioId':['',[Validators.required]]
   });
+
   constructor(
     private formBuilder: FormBuilder,
     private apartamentoService: ApartamentoService,
-    private router : Router
+    private router : Router,
+    private torreService: TorreService,
+    private propietarioService: PropietarioService
   ) { }
 
   ngOnInit(): void {
@@ -43,4 +53,23 @@ export class CrearApartamentoComponent implements OnInit {
       });
   }
 
+  getTorre(){
+    this.torreService.getTorres()
+      .subscribe({
+        next: (torre) => {
+          this.listadoTorre = torre;
+        },
+        error: (error) => console.log("Error al consultar la torre")
+      })
+  }
+
+  getPropietario(){
+    this.propietarioService.getPropietario()
+      .subscribe({
+        next: (propietario) => {
+          this.listadoPropietarios = propietario;
+        },
+        error: (error) => console.log("Error al consultar el propietario")
+      })
+  }
 }
